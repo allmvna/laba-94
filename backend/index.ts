f9b1e59174dc2748ea899d469de8d69cf91d0396
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2025-02-21 17:02:50 +0600

    added a user with the ability to register, log in, and logout

---
+++ backend/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from "mongoose";
+import mongoDb from "./mongoDb";
+import userRouter from "./routes/user/user";
+import config from "./config";
+import path from "path";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/users', userRouter);
+app.use(express.static('public'));
+app.use('/fixtures', express.static(path.join(__dirname, 'fixtures')));
+
+const run = async () => {
+    await mongoose.connect(config.db);
+
+    app.listen(port, () => {
+        console.log(`Server started on http://localhost:${port}`);
+    });
+
+    process.on('exit', err => {
+        mongoDb.disconnect();
+    })
+};
+
+
+run().catch(e => console.error(e));
+
+
